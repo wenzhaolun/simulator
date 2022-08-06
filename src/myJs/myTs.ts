a47699f905a65776c86c6719632db89904dae527
wenzhaolun <97371188+wenzhaolun@users.noreply.github.com>
2022-08-06 20:41:00 +0800

    update

--- src/myJs/myTs.ts
+++ src/myJs/myTs.ts
@@ -14,12 +14,33 @@ interface UserProfile{
 class User{
     public profile: UserProfile
     public itemArray: Array<Item> = []
-    
+    pickItem(x: Item) {
+        this.itemArray = this.itemArray.concat(x)
+    }
+    dropItem(x: Item) {
+        const id = x.profile.id
+        
+    }
     constructor(a: UserProfile, b: Array<Item>) {
         this.profile = a
         this.itemArray = this.itemArray.concat(b)
     }
 }
 
+interface ItemProfile{
+    id: number,
+    name: string,
+    intro: string,
+    atk: number,
+    def: number,
+    hp: number,
+    life: number, // 道具可使用时间
+    weight: number
+}
+
 class Item {
+    public profile: ItemProfile
+    constructor(a: ItemProfile) {
+        this.profile = a
+    }
 }
