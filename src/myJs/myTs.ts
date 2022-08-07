ea5c590a4478ea5ebff53b1a5d1b75bf06a4c8e3
wenzhaolun <97371188+wenzhaolun@users.noreply.github.com>
2022-08-07 16:00:51 +0800

    update

--- src/myJs/myTs.ts
+++ src/myJs/myTs.ts
@@ -3,16 +3,18 @@ export function myTestFunction(x: string): number {
     return 1
 }
 
-interface UserProfile{
-    strength: number,
-    agility: number,
-    intelligence: number,
-    sans: number,
-    sight: number
+const userBasic = {
+    sight: 40
 }
 
 class User{
-    public profile: UserProfile
+    public profile: {
+        strength: number,
+        agility: number,
+        intelligence: number,
+        sans: number,
+        sight: number
+    }
     public itemArray: Array<Item> = []
     pickItem(x: Item) {
         this.itemArray = this.itemArray.concat(x)
@@ -29,8 +31,12 @@ class User{
     useItem(x: Item) {
         const id = x.profile.id
     }
-    constructor(a: UserProfile, b: Array<Item>) {
-        this.profile = a
+    constructor(strength, agility, intelligence, sans, b: Array<Item>) {
+        this.profile.strength = strength
+        this.profile.agility = agility
+        this.profile.intelligence = intelligence
+        this.profile.sans = sans
+        this.profile.sight = userBasic.sight
         this.itemArray = b.concat(this.itemArray)
     }
 }
